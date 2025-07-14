45c7f14e9bb4ae08208e88c0fde535697a87f906
BerivanAyaz <berivannayaz@gmail.com>
2025-07-14 22:04:00 +0300

    feat: Align project with original Yarn setup

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,50 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import jsconfigPaths from 'vite-jsconfig-paths';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // --- YENİ EKLENEN KISIM BURASI ---
-  // Derleme (build) ayarları
-  build: {
-    rollupOptions: {
-      // 'external' dizisi, derleyicinin hangi paketleri
-      // kodun içine dahil etmeye ÇALIŞMAMASI gerektiğini söyler.
-      external: ['pandas']
-    }
-  }
+export default defineConfig(({ mode }) => {
+  // depending on your application, base can also be "/"
+  const env = loadEnv(mode, process.cwd(), '');
+  const API_URL = `${env.VITE_APP_BASE_NAME}`;
+  const PORT = 3000;
+
+  return {
+    server: {
+      // this ensures that the browser opens upon server start
+      open: true,
+      // this sets a default port to 3000
+      port: PORT,
+      host: true
+    },
+    build: {
+      chunkSizeWarningLimit: 1600
+    },
+    preview: {
+      open: true,
+      host: true
+    },
+    define: {
+      global: 'window'
+    },
+    resolve: {
+      alias: {
+        // { find: '', replacement: path.resolve(__dirname, 'src') },
+        // {
+        //   find: /^~(.+)/,
+        //   replacement: path.join(process.cwd(), 'node_modules/$1')
+        // },
+        // {
+        //   find: /^src(.+)/,
+        //   replacement: path.join(process.cwd(), 'src/$1')
+        // }
+        // {
+        //   find: 'assets',
+        //   replacement: path.join(process.cwd(), 'src/assets')
+        // },
+        '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs'
+      }
+    },
+    base: API_URL,
+    plugins: [react(), jsconfigPaths()]
+  };
 });
